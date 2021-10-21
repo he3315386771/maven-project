0e4c874c3ae068f7077ea2df226d8df5d3fe9188
he3315386771 <3315386771@qq.com>
2021-10-21 15:35:51 +0800

    工具类更新

--- javaSE/src/main/java/util/CollectionUtil.java
+++ javaSE/src/main/java/util/CollectionUtil.java
@@ -3,9 +3,11 @@ package util;
 import org.apache.commons.collections.CollectionUtils;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class CollectionUtil extends CollectionUtils {
     private CollectionUtil(){}
@@ -33,5 +35,61 @@ public class CollectionUtil extends CollectionUtils {
         }else{
             return new ArrayList(Arrays.asList(array));
         }
+    }
+
+    /**
+     * 去重
+     * @param list
+     * @param keyExtractors
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> distinct(List<T> list, Function<? super T, Object>... keyExtractors){
+        return list.stream().filter(distinctByKey(keyExtractors)).collect(Collectors.toList());
+    }
+    private static <T> Predicate<T> distinctByKey(Function<? super T, Object>... keyExtractors) {
+        if(keyExtractors==null || keyExtractors.length==0){
+            Set<Object> seen = ConcurrentHashMap.newKeySet();
+            return t -> seen.add(t);
+        }
+        List<List<Object>> lists = new ArrayList<>(keyExtractors.length);
+        for(int i = 0 ;i<keyExtractors.length; i++){
+            lists.add(new ArrayList<>());
+        }
+        return t -> {
+            Boolean flag=true;
+            Set<Integer> set = new HashSet<>();
+            for(int i = 0 ;i<lists.get(0).size(); i++){
+                set.add(i);
+            }
+            for(int i =0 ;i<keyExtractors.length;i++){
+                Boolean flag2=true;
+                Iterator<Integer> setI = set.iterator();
+                while(setI.hasNext()){
+                    int j = setI.next();
+                    if(i==keyExtractors.length-1){
+                        if(lists.get(i).get(j).equals(keyExtractors[i].apply(t))){
+                            flag=false;
+                            break;
+                        }
+                    }else{
+                        if((!lists.get(i).get(j).equals(keyExtractors[i].apply(t)))){
+                            setI.remove();
+                        }else{
+                            flag2 = false;
+                        }
+                    }
+                }
+                if(flag2 || !flag){
+                    break;
+                }
+            }
+            if(flag){
+                for(int i =0 ;i<keyExtractors.length;i++){
+                    lists.get(i).add(keyExtractors[i].apply(t));
+                }
+            }
+            return flag;
+        };
     }
 }
