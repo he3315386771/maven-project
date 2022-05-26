7bb78d91e2ef9851320487ceefc375fc65f0acbd
he3315386771 <3315386771@qq.com>
2022-05-26 14:56:27 +0800

    新增xml解析

---
+++ javaSE/src/test/java/Xml.java
@@ -0,0 +1,14 @@
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.dom4j.Document;
+import org.dom4j.io.SAXReader;
+
+import java.io.ByteArrayInputStream;
+
+public class Xml {
+    public static void main(String[] args) throws Exception{
+        String xml = "&lt;Root&gt;&lt;Succ&gt;true&lt;/Succ&gt;&lt;Msg&gt;&lt;/Msg&gt;&lt;ResponseInfo&gt;&lt;Data&gt;&lt;dept_code&gt;0253&lt;/dept_code&gt;&lt;date_birth&gt;1964-11-06&lt;/date_birth&gt;&lt;visit_no&gt;&lt;/visit_no&gt;&lt;contact_phone&gt;18685043246&lt;/contact_phone&gt;&lt;nation&gt;汉族&lt;/nation&gt;&lt;marry&gt;未婚&lt;/marry&gt;&lt;remark&gt;&lt;/remark&gt;&lt;doctor_id&gt;&lt;/doctor_id&gt;&lt;time_discr&gt;&lt;/time_discr&gt;&lt;patient_name&gt;王长发&lt;/patient_name&gt;&lt;cancel_reg_flag&gt;0&lt;/cancel_reg_flag&gt;&lt;doctor_name&gt;&lt;/doctor_name&gt;&lt;visit_status&gt;就诊结束&lt;/visit_status&gt;&lt;address&gt;贵州贵阳&lt;/address&gt;&lt;sex&gt;男&lt;/sex&gt;&lt;dept_name&gt;血液透析门诊&lt;/dept_name&gt;&lt;weight&gt;&lt;/weight&gt;&lt;c_whcd&gt;&lt;/c_whcd&gt;&lt;cardno&gt;38621695&lt;/cardno&gt;&lt;visit_number&gt;&lt;/visit_number&gt;&lt;identityno&gt;520102196411061615&lt;/identityno&gt;&lt;regist_time&gt;%s&lt;/regist_time&gt;&lt;c_zy&gt;工人&lt;/c_zy&gt;&lt;patient_id&gt;38621695&lt;/patient_id&gt;&lt;dept_id&gt;195&lt;/dept_id&gt;&lt;visit_date&gt;%s&lt;/visit_date&gt;&lt;age&gt;56岁&lt;/age&gt;&lt;doctor_code&gt;&lt;/doctor_code&gt;&lt;/Data&gt;&lt;Data&gt;&lt;dept_code&gt;0253&lt;/dept_code&gt;&lt;date_birth&gt;2020-11-06&lt;/date_birth&gt;&lt;visit_no&gt;&lt;/visit_no&gt;&lt;contact_phone&gt;18685043246&lt;/contact_phone&gt;&lt;nation&gt;汉族&lt;/nation&gt;&lt;marry&gt;未婚&lt;/marry&gt;&lt;remark&gt;&lt;/remark&gt;&lt;doctor_id&gt;100058&lt;/doctor_id&gt;&lt;time_discr&gt;&lt;/time_discr&gt;&lt;patient_name&gt;测试A&lt;/patient_name&gt;&lt;cancel_reg_flag&gt;0&lt;/cancel_reg_flag&gt;&lt;doctor_name&gt;杨睿&lt;/doctor_name&gt;&lt;visit_status&gt;就诊结束&lt;/visit_status&gt;&lt;address&gt;贵州贵阳&lt;/address&gt;&lt;sex&gt;男&lt;/sex&gt;&lt;dept_name&gt;内分泌科主任医师门诊_3楼&lt;/dept_name&gt;&lt;weight&gt;&lt;/weight&gt;&lt;c_whcd&gt;&lt;/c_whcd&gt;&lt;cardno&gt;7654321&lt;/cardno&gt;&lt;visit_number&gt;&lt;/visit_number&gt;&lt;identityno&gt;520102196411061615&lt;/identityno&gt;&lt;regist_time&gt;%s&lt;/regist_time&gt;&lt;c_zy&gt;工人&lt;/c_zy&gt;&lt;patient_id&gt;7654321&lt;/patient_id&gt;&lt;dept_id&gt;92&lt;/dept_id&gt;&lt;visit_date&gt;%s&lt;/visit_date&gt;&lt;age&gt;56岁&lt;/age&gt;&lt;doctor_code&gt;489&lt;/doctor_code&gt;&lt;/Data&gt;&lt;/ResponseInfo&gt;&lt;MsgType&gt;B001&lt;/MsgType&gt;&lt;/Root&gt;";
+        String sml = StringEscapeUtils.unescapeXml(xml);
+        Document doc = new SAXReader().read(new ByteArrayInputStream(sml.getBytes()));
+        Object sml1 = doc.selectNodes("//Data");
+    }
+}
